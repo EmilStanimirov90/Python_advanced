8a066a14d9022d8951f8bd49707968aa83903afc
Emil Stanimirov <145929264+EmilStanimirov90@users.noreply.github.com>
2023-10-03 11:16:07 +0300

    exercise 4

--- 02_tuples_and_sets_exercise/03_periodic_table_exercise.py
+++ 02_tuples_and_sets_exercise/03_periodic_table_exercise.py
@@ -1,7 +1,12 @@
 n = int(input())
 unique_elements = set()
+
 for _ in range(n):
-    current_elements = (input().split())
-    for element in current_elements:
-        unique_elements.add(element)
+
+    unique_elements.update([el for el in input().split()])
+
+#     current_elements = (input().split())
+#     for element in current_elements:
+#         unique_elements.add(element)
+
 print(*unique_elements, sep="\n")
--- 02_tuples_and_sets_exercise/04_count_symbols_exercise.py
+++ 02_tuples_and_sets_exercise/04_count_symbols_exercise.py
@@ -1,2 +1,19 @@
-a = set([1,23,4,5,6,6,6,54,3,2,3])
-print(a)
+# text = list(input())
+#
+# unique_symbols = sorted(set(text))
+#
+# for symbol in unique_symbols:
+#     print(f"{symbol}: {text.count(symbol)} time/s")
+#
+text = list(input())
+occurrences = {}
+for symbol in text:
+    if symbol not in occurrences:
+        occurrences[symbol] = 1
+    else:
+        occurrences[symbol] += 1
+sorted_items = sorted(occurrences.items())
+sorted_dict = dict(sorted_items)
+
+for char, qty in sorted_dict.items():
+    print(f"{char}: {qty} time/s")
