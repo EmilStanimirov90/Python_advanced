ad998e0fecf1eb58fa6603d7d8859e38de50f5ac
Emil Stanimirov <145929264+EmilStanimirov90@users.noreply.github.com>
2023-11-14 23:42:03 +0200

    exercise game tic tac toe

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
-list = [1, 3]
-matrix = [[4,5], [6,7]]
-print(list[0])
-print(matrix[list[0]][list[0]])
+# list = [1, 3]
+# matrix = [[4,5], [6,7]]
+# print(list[0])
+# print(matrix[list[0]][list[0]])
+
